4b98ec0c5c92304b64e6974d2517f9ad3fbc532f
domingos <mds.junior87@gmail.com>
2025-05-03 16:08:41 -0300

    chore: remove .NET build artifacts and ASP.NET files

--- Models/Tweet.cs
+++
@@ -1,9 +0,0 @@
-namespace FuriaKnowYourFan.Models
-{
-    public class Tweet
-    {
-        public string? Id { get; set; }
-        public string? Text { get; set; }
-        public string? CreatedAt { get; set; }
-    }
-}
