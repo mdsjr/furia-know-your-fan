3721158e30c4b1f21e4eb3ae41ec669211154165
domingos <mds.junior87@gmail.com>
2025-05-02 22:07:12 -0300

    ♻️ :recycle:

--- obj/Debug/net9.0/FuriaKnowYourFan.AssemblyInfo.cs
+++ obj/Debug/net9.0/FuriaKnowYourFan.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FuriaKnowYourFan")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb781443c2ad7cd0bb2d8844df8ac823ed5e23a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52f26c715c44c83cf0937719e255cdbbf5bb0df9")]
 [assembly: System.Reflection.AssemblyProductAttribute("FuriaKnowYourFan")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FuriaKnowYourFan")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
